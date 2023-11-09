def39ad13e78c52bab59721bd1d7d55bd6624103
MosheYitzhakov <126177360+MosheYitzhakov@users.noreply.github.com>
2023-11-09 18:43:53 +0200

    aaaa

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require("path")
+const fs = require("fs/promises");
 const app = express();
 const GRouter = require('./routers/get')
 const PutRouter = require('./routers/put')
@@ -9,8 +10,15 @@ const cors = require('cors');
 app.use(express.json())
 app.use(cors());
 app.use(express.static(path.join(path.dirname(__dirname), "drive", "build")))
-app.use((req,res, next) => {
+app.use(async(req,res, next) => {
     console.log(req.url);
+    try {
+        await fs.access(path.join(__dirname, "userData", req.url.replace("/api", "")))
+        console.log("exist");
+    } catch(e) {
+        console.log(e.message);
+    }
+    // const flag = fs.
     next();
 })
 app.use("/api" ,express.static('userData'))
